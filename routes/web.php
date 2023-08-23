30233c6d20c8d9311ebfd33562df60620640e623
NoBody313 <muhammadfatihmaulana434@gmail.com>
2023-08-23 16:32:29 +0700

    Fix Aman 🥳

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', [UsersController::class, 'index'])->name('users.index');
+Route::post('/users/import', [UsersController::class, 'import'])->name('users.import');
+Route::get('/users/export', [UsersController::class, 'export'])->name('users.export');
